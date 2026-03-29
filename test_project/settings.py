74e90cf63640f0aaef41023403d2f656d6549856
myomyintaung <myomyintaung@gmail.com>
2026-03-29 20:21:30 +0800

    added cloudinary

--- test_project/settings.py
+++ test_project/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'test_app',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -142,6 +143,12 @@ STORAGES = {
     },
 }
 
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.getenv('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.getenv('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.getenv('CLOUDINARY_API_SECRET')
+}
 
 
 
